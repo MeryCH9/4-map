4d8e5fc7bc49d5b134fe3825dc7d37203f5bb293
Mery <104753438+MeryCH9@users.noreply.github.com>
2022-12-16 23:56:40 +0000

    MeryCH

--- Bigrams.java
+++ Bigrams.java
@@ -1,77 +1,122 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Bigrams {
 
-	public static class Pair<T1, T2> {
-		public T1 first;
-		public T2 second;
+    public static class Pair<T1, T2> {
+        public T1 first;
+        public T2 second;
         public int hashCode;
-		public Pair(T1 first, T2 second) {
-			this.first = first;
-			this.second = second;
-		    this.hashCode = Objects.hash(first, second);
-		}
+        public Pair(T1 first, T2 second) {
+            this.first = first;
+            this.second = second;
+            this.hashCode = Objects.hash(first, second);
+        }
 
-		@Override
-		public boolean equals(Object other) {
-		    if (this == other) return true;
-		    if (other == null || getClass() != other.getClass()) return false;
-		    Pair<String, String> otherPair = (Pair<String, String>) other;
-		    return this.first.equals(otherPair.first) && this.second.equals(otherPair.second);
-		}
+        @Override
+        public boolean equals(Object other) {
+            if (this == other) return true;
+            if (other == null || getClass() != other.getClass()) return false;
+            Pair<String, String> otherPair = (Pair<String, String>) other;
+            return this.first.equals(otherPair.first) && this.second.equals(otherPair.second);
+        }
 
-		@Override
-		public int hashCode() { return this.hashCode; }
-	}
+        @Override
+        public int hashCode() { return this.hashCode; }
+    }
 
-	protected Map<Pair<String, String>, Float> bigramCounts;
-	protected Map<String, Float> unigramCounts;
+    protected Map<Pair<String, String>, Float> bigramCounts;
+    protected Map<String, Float> unigramCounts;
 
-	// TODO: Given filename fn, read in the file word by word
-	// For each word:
-	// 1. call process(word)
-	// 2. increment count of that word in unigramCounts
-	// 3. increment count of new Pair(prevword, word) in bigramCounts
+    // TODO: Given filename fn, read in the file word by word
+    // For each word:
+    // 1. call process(word)
+    // 2. increment count of that word in unigramCounts
+    // 3. increment count of new Pair(prevword, word) in bigramCounts
 	public Bigrams(String fn) {
+        bigramCounts = new HashMap<>();
+        unigramCounts = new HashMap<>();
+        try {
+            Scanner sc = new Scanner(new File(fn));
+            String prevWord = null;
+            while (sc.hasNext()) {
+                String word = sc.next();
+                word = process(word);
+                if (unigramCounts.containsKey(word)){
+                    unigramCounts.put(word, unigramCounts.get(word) + 1);
+                } else {
+                    unigramCounts.put(word, 1f);
+                }
+                if (prevWord != null) {
+                    Pair<String, String> p = new Pair<>(prevWord, word);
+                    if (bigramCounts.containsKey(p)){
+                        bigramCounts.put(p, bigramCounts.get(p) + 1);
+                    } else {
+                        bigramCounts.put(p, 1f);
+                    }                }
+                prevWord = word;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
-	}
+    // TODO: Given words w1 and w2,
+    // 1. replace w1 and w2 with process(w1) and process(w2)
+    // 2. print the words
+    // 3. if bigram(w1, w2) is not found, print "Bigram not found"
+    // 4. print how many times w1 appears
+    // 5. print how many times (w1, w2) appears
+    // 6. print count(w1, w2)/count(w1)
+    public float lookupBigram(String w1, String w2) {
+		w1 = process(w1);
+		w2 = process(w2);
 
-	// TODO: Given words w1 and w2,
-	// 1. replace w1 and w2 with process(w1) and process(w2)
-	// 2. print the words
-	// 3. if bigram(w1, w2) is not found, print "Bigram not found"
-	// 4. print how many times w1 appears
-	// 5. print how many times (w1, w2) appears
-	// 6. print count(w1, w2)/count(w1)
-	public float lookupBigram(String w1, String w2) {
-		return (float) 0.0;
-	}
+		System.out.println("(" + w1 + ", " + w2 + ")");
 
-	protected String process(String str) {
-		return str.toLowerCase().replaceAll("[^a-z]", "");
-	}
+		Pair<String, String> p = new Pair<>(w1, w2);
+        if (!bigramCounts.containsKey(p)) {
+			System.out.println("Bigram not found.");
+			System.out.printf(w1 + " appears %.2f times. \n" , unigramCounts.get(w1));
+            if(bigramCounts.containsKey(p)){
+                System.out.printf("(" + w1 + ", " + w2 + ") appears %.2f times\n" , bigramCounts.get(p));
+            }else{
+                System.out.printf("(" + w1 + ", " + w2 + ") appears %.2f times\n", 0.0f);
+                System.out.printf("%.4f\n" , 0.0f);                     
+            }
+            return 0;
+        }
+	System.out.printf(w1+ " appears %.2f times. \n",unigramCounts.get(w1));
+	System.out.printf("(" + w1 + ", " + w2 + ") appears %.2f times. \n",bigramCounts.get(p));
+	System.out.printf("%.4f \n", (float) bigramCounts.get(p) / unigramCounts.get(w1));
+    return (float) 0.0;
+}
 
-	public static void main(String[] args) {
-		if (args.length != 1) {
-			System.out.println("Usage: java Bigrams <FILENAME>");
-			System.out.println(args.length);
-			return;
-		}
+    protected String process(String str) {
+        return str.toLowerCase().replaceAll("[^a-z]", "");
+    }
 
-		Bigrams bg = new Bigrams(args[0]);
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage: java Bigrams <FILENAME>");
+            System.out.println(args.length);
+            return;
+        }
 
-		List<Pair<String, String>> wordpairs = Arrays.asList(
-			new Pair("with", "me"), 
-			new Pair("the", "grass"),
-			new Pair("the", "king"),
-			new Pair("to", "you")
-		);
+        Bigrams bg = new Bigrams(args[0]);
 
-		for (Pair<String, String> p : wordpairs) {
-			bg.lookupBigram(p.first, p.second);
-		}
+        List<Pair<String, String>> wordpairs = Arrays.asList(
+                new Pair("with", "me"),
+                new Pair("the", "grass"),
+                new Pair("the", "king"),
+                new Pair("to", "you")
+        );
 
-		System.out.println(bg.process("adddaWEFEF38234---+"));
-	}
+        for (Pair<String, String> p : wordpairs) {
+            bg.lookupBigram(p.first, p.second);
+        }
 
+        System.out.println(bg.process("adddaWEFEF38234---+"));
+    }
 }
